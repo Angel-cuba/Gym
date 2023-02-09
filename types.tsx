a5d903e1b20573366389223b5cad392f67b7bee8
Mac_Developer <araozangel842@gmail.com>
2023-02-09 21:57:17 +0200

    Refactoring code

--- types.tsx
+++ types.tsx
@@ -25,8 +25,10 @@ export type RootStackScreenProps<Screen extends keyof RootStackParamList> = Nati
 >;
 
 export type RootTabParamList = {
-  TabOne: undefined;
-  TabTwo: undefined;
+  Home: undefined;
+  Favourites: undefined;
+  Trending: undefined;
+  Profile: undefined;
 };
 
 export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<
