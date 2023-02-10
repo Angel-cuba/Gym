afcd62dc50d9ce2b504b150efc7d7d4654f24456
Mac_Developer <araozangel842@gmail.com>
2023-02-10 21:10:28 +0200

    Add styles and navigation method

--- auth/views/LoginScreen.tsx
+++ auth/views/LoginScreen.tsx
@@ -1,10 +1,87 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, SafeAreaView, Pressable } from 'react-native';
+import React from 'react';
+import { BigText } from '../../components/StyledText';
+import CustomInput from '../../components/StyledInput';
+import CustomButton from '../../components/StyledButton';
+import Separator from '../../components/Separator';
 
-export default function LoginScreen() {
+export default function LoginScreen({ navigation}: any) {
+ const goToSignUp = () => {
+    navigation.navigate('Register')
+  }
+
+
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
-  )
+    <SafeAreaView style={styles.container}>
+      <BigText big bold color="login">
+        LoginScreen
+      </BigText>
+      <View style={styles.inputsView}>
+        <CustomInput
+          label="Email"
+          placeholder="Enter your email"
+          value=""
+          onChangeText={(text) => {
+            console.log(text);
+          }}
+        />
+        <CustomInput
+          label="Password"
+          placeholder="Enter your password"
+          value=""
+          onChangeText={(text) => {
+            console.log(text);
+          }}
+        />
+      </View>
+      <Pressable
+      style={styles.signup}
+        onPress={goToSignUp}
+      >
+        <Text>
+          Don't have an account?
+          <Text style={{ color: '#ff0000' }}> Sign Up</Text>
+        </Text>
+      </Pressable>
+
+      <CustomButton
+        label="Login"
+        backgroundColor="#ff0000"
+        radius={10}
+        fontSize={20}
+        onPress={() => {
+          console.log('Login');
+        }}
+      />
+
+      <Separator />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 150,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+  inputsView: {
+    width: '100%',
+    alignItems: 'center',
+    marginTop: 150,
+  },
+  signup: {
+    marginTop: 10,
+    marginBottom: 35,
+  }
+});
