9b5eabcdd9aef2a5c8bf45aaa6d816c480fde5d6
Mac_Developer <araozangel842@gmail.com>
2023-02-11 21:33:34 +0200

    Hide password method

--- components/StyledInput.tsx
+++ components/StyledInput.tsx
@@ -1,17 +1,32 @@
-import { TextInput, StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Pressable } from 'react-native';
+import { TextInput } from 'react-native-paper';
 import React from 'react';
 import { StyledText } from './StyledText';
+import { FontAwesome } from '@expo/vector-icons';
 
 type CustomInputProps = {
   type?: string;
   placeholder: string;
   value: string;
   onChangeText: (text: string) => void;
+  autoCapitalize?: 'none' | 'sentences' | 'words' | 'characters';
   label: string;
   color?: string;
+  secureTextEntry?: boolean;
+  right?: boolean;
 };
 
-const CustomInput = ({ placeholder, value, onChangeText, label, color }: CustomInputProps) => {
+const CustomInput = ({
+  placeholder,
+  value,
+  onChangeText,
+  autoCapitalize,
+  label,
+  color,
+  secureTextEntry,
+  right,
+}: CustomInputProps) => {
+  const [passwordVisible, setPasswordVisible] = React.useState(true);
   return (
     <View style={{ width: '100%', alignItems: 'center' }}>
       <StyledText
@@ -28,8 +43,22 @@ const CustomInput = ({ placeholder, value, onChangeText, label, color }: CustomI
         placeholder={placeholder}
         value={value}
         onChangeText={onChangeText}
+        autoCapitalize={autoCapitalize || 'none'}
         cursorColor="#ff0000"
+        secureTextEntry={secureTextEntry && passwordVisible}
+        right={right &&
+          <TextInput.Icon
+            icon={passwordVisible ? 'eye' : 'eye-off'}
+            onPress={() => setPasswordVisible(!passwordVisible)}
+          />
+        }
       />
+      {/* <Pressable>
+        <FontAwesome
+          name={passwordVisible ? 'eye' : 'eye-slash'}
+          onPress={() => setPasswordVisible(!passwordVisible)}
+        />
+      </Pressable> */}
     </View>
   );
 };
