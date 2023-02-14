d60e41e08cca4716cc3960c1d3913abc2225ee91
Mac_Developer <araozangel842@gmail.com>
2023-02-15 01:44:17 +0200

    Add update current user method
    DisplayName and PhotoURL are now editable

--- views/Profile.tsx
+++ views/Profile.tsx
@@ -3,46 +3,51 @@ import React, { useId } from 'react';
 import { getAuth, updateProfile } from 'firebase/auth';
 import CustomInput from '../components/StyledInput';
 import CustomButton from '../components/StyledButton';
+import ImageView from '../components/profile/ImageView';
+import * as ImagePicker from 'expo-image-picker';
 
+// Deafault image from cloudinary
+  const imageUrlFromCloudinary = 'https://res.cloudinary.com/dqaerysgb/image/upload/v1648218398/istockphoto-1132926013-612x612_t1xwec.jpg'
 
 export default function ProfileScreen() {
   const [user, setUser] = React.useState<any>(null);
-  const auth = getAuth();
-  // console.log('user', user);
-
+  console.log('user', user);
   const [name, setName] = React.useState('');
-  const [phone, setPhone] = React.useState('');
   const [photo, setPhoto] = React.useState('');
+  const loggedUser = getAuth().currentUser;
 
   React.useEffect(() => {
-    const authUser = getAuth()?.currentUser;
-    if (authUser) {
-      setUser(authUser as any);
-      console.log('authUser', authUser.uid);
-    }
+    setUser(loggedUser);
   }, []);
 
-  const handleText = (text: string) => {
-    console.log(text);
-    //auth.updateCurrentUser({})
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
 
+    if (!result.canceled) {
+      setPhoto(result.assets[0].uri);
+    } else {
+      alert('You did not select any image.');
+    }
   };
 
-  const handleUpdate = () => {
-    console.log('update', name, phone, photo);
+  const handleUpdate = async () => {
     try {
-      if(user) {
-        updateProfile(user, {
-      displayName: name,
-      photoURL: photo,
-      // phoneNumber: phone,
-    }).then(() => {
-      console.log('update success');
-    }
-    ).catch((error) => {
-      console.log('error', error);
-    }
-    ); 
+      if (user) {
+        await updateProfile(user, {
+          displayName: name,
+          photoURL: photo,
+        })
+          .then(() => {
+            console.log('update success');
+          })
+          .catch((error) => {
+            console.log('error', error);
+          });
       }
     } catch (error) {
       console.log('error', error);
@@ -51,34 +56,29 @@ export default function ProfileScreen() {
 
   return (
     <View style={styles.container}>
-      {/* <CustomInput
-        label={`${user?.email}`}
-        value={`${user?.email}`}
-        placeholder={`${user?.email}`}
-        onChangeText={handleText}
-      /> */}
-      <Text>{user.email}</Text>
+     <View style={styles.imageSection}>
+      {/* <Text style={{ backgroundColor: 'red', color: 'black' }}>
+        ultimo login --{user?.lastLoginAt}
+      </Text> */}
+          <ImageView placeHolderImageView={imageUrlFromCloudinary} selectedImage={user?.photoURL} width={100} height={100} radius={50}/>
+       <Text style={styles.email}>{user?.email}</Text>
+     </View>
       <CustomInput
         label={`${user?.displayName}`}
         value={name}
-        placeholder={`${user?.displayName}`}
+        placeholder={user?.displayName ? 'Give new name...' : 'Enter your name'}
         onChangeText={(text) => setName(text)}
       />
-      <CustomInput
-        label={`${user?.phoneNumber}`}
-        value={phone}
-        placeholder={`${user?.phoneNumber}`}
-        onChangeText={(text) => setPhone(text)}
-      />
-      <CustomInput
-        label={`${user?.photoURL}`}
-        value={photo}
-        placeholder={`${user?.photoURL}`}
-        onChangeText={(text) => setPhoto(text)}
-      />
-      <Text style={{ backgroundColor: 'red', color: 'black' }}>creado --{user?.createdAt}</Text>
-      <Text style={{ backgroundColor: 'red', color: 'black' }}>ultimo login --{user?.lastLoginAt}</Text>  
-      <CustomButton label='Update user' onPress={handleUpdate} radius={10}/>
+
+          <ImageView placeHolderImageView={imageUrlFromCloudinary} selectedImage={photo} width={200} height={200} radius={10}/>
+          <CustomButton
+            label={user?.photoURL ? 'Update image' : 'Select an image'}
+            onPress={pickImageAsync}
+            backgroundColor="#343434"
+            radius={10}
+          />
+   
+      <CustomButton label="Update user" onPress={handleUpdate} radius={10} />
     </View>
   );
 }
@@ -88,4 +88,16 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
   },
+  imageSection: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    margin: 10,
+    width: '90%',
+  },
+  email:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 10,
+  }
 });
