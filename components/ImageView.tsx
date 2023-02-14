de716ce56dea68f95ff9db787bc3fb2ae66a5a89
Mac_Developer <araozangel842@gmail.com>
2023-02-15 01:42:15 +0200

    Add image view for image

--- components/ImageView.tsx
+++
@@ -1,8 +0,0 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
-
-export default function ImageView({placeHolderImageView, selectedImage} : any) {
-const imageSource = selectedImage ? {uri: selectedImage} : placeHolderImageView
-
-  return <Image source={imageSource} style={{width: 200, height: 200}} />
-}
---
+++ components/profile/ImageView.tsx
@@ -0,0 +1,8 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+
+export default function ImageView({placeHolderImageView, selectedImage, width, height, radius} : any) {
+const imageSource = selectedImage ? {uri: selectedImage} : {uri: placeHolderImageView}
+
+  return <Image source={imageSource} style={{width: width, height: height, borderRadius: radius, margin: 10}} />
+}
