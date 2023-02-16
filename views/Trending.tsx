6dd510e079bc0f9ed60eb2c8b214133a779031bc
Mac_Developer <araozangel842@gmail.com>
2023-02-16 20:08:38 +0200

    Add bg color

--- views/Trending.tsx
+++ views/Trending.tsx
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import { RootTabScreenProps } from '../types';
 
-export default function TrendingScreen() {
+export default function TrendingScreen({ route}: RootTabScreenProps<'Trending'>) {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>TrendingScreen</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+});
