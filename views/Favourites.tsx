72518d059c17b6562b7714671f4e4ec8c3748278
Mac_Developer <araozangel842@gmail.com>
2023-02-19 17:20:58 +0200

    Add loading and empty method states with animation

--- views/Favourites.tsx
+++ views/Favourites.tsx
@@ -6,17 +6,23 @@ import { StyledText } from '../components/StyledText';
 import { View } from '../components/Themed';
 import { app } from '../firebase';
 import { fetchingData, optionsUrl, urlById } from '../utils/queries';
-
+import LottieView from 'lottie-react-native';
+import Loading from '../components/Loading';
 
 const screenWidth = Math.round(Dimensions.get('window').width);
 
 export default function FavouritesScreen() {
   const [favouritesSelected, setFavouritesSelected] = React.useState<any>([]);
+  const [loading, setLoading] = React.useState(false);
+
   //!Get user id from firebase
   const user = getAuth().currentUser;
   const userId = user?.uid;
   const db = getFirestore(app);
 
+  React.useEffect(() => {
+    getData();
+  }, []);
   const getData = async () => {
     //? Get all documents from a collection
     const dataFromFirebase = (await getDocs(collection(db, 'favourites'))).docs.map((doc) =>
@@ -29,18 +35,38 @@ export default function FavouritesScreen() {
     console.log('cantidad de docs', filteredFavourites.length);
     console.log('cantidad de docs', filteredFavourites);
     setFavouritesSelected(filteredFavourites[0]);
+    setLoading(false);
   };
 
-  React.useEffect(() => {
-    getData();
-  }, []);
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <View style={styles.container}>
-      <StyledText big bold>
-        Favourites
-      </StyledText>
-      {!favouritesSelected.selected ? (
-        <StyledText> No favourites </StyledText>
+      {!favouritesSelected?.selected ? (
+        <View style={{ alignItems: 'center', marginTop: 50 }}>
+          <StyledText big bold color="part">
+            {' '}
+            Nothing to show here
+          </StyledText>
+          <View style={{
+            width: screenWidth - 100,
+          }}>
+            <StyledText style={{
+              textAlign: 'center',
+            }}>
+              Check some exercises and if you like it, give it a like and it will appear here!
+            </StyledText>
+          </View>
+          <LottieView
+            source={require('../assets/lottie/loading-paperplane.json')}
+            autoPlay
+            loop
+            style={{ width: 200, height: 300, aspectRatio: 1 }}
+            duration={3000}
+          />
+        </View>
       ) : (
         favouritesSelected.selected.map((id: any) => <FavouriteItem key={id} id={id} />)
       )}
@@ -64,22 +90,26 @@ const FavouriteItem = ({ id }: any) => {
   };
   return (
     <View>
-      <Card item={favourite}/>
+      <Card item={favourite} />
     </View>
   );
 };
 
 const Card = ({ item }: any) => {
   return (
-    <View style={{
-      width: screenWidth - 40,
-      backgroundColor: '#0057b4c7',
-      borderRadius: 10,
-      marginVertical: 10,
-
-    }}>
+    <View
+      style={{
+        width: screenWidth - 40,
+        backgroundColor: '#0057b4c7',
+        borderRadius: 10,
+        marginVertical: 10,
+      }}
+    >
       <Image source={{ uri: item.gifUrl }} style={{ width: 150, height: 150 }} />
-      <StyledText big color=''> {item.name} </StyledText>
+      <StyledText big color="">
+        {' '}
+        {item.name}{' '}
+      </StyledText>
     </View>
   );
 };
