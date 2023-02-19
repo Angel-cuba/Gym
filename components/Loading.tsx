4ebe91e625dabdeeb75ae10a809f211285d3d1f8
Mac_Developer <araozangel842@gmail.com>
2023-02-19 17:19:51 +0200

    Build animated loading screen

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,10 +1,27 @@
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
+import LottieView from 'lottie-react-native'
 
 export default function Loading() {
+const animation = '../assets/lottie/bloks.json';
   return (
-    <View>
-      <Text>Loading</Text>
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#ffffff11',
+    }}>
+      <LottieView
+      source={require(animation)}
+      autoPlay
+      loop
+      style={{
+        width: 500,
+        height: 200,
+        aspectRatio: 1,
+      }}
+
+      />
     </View>
   )
 }
