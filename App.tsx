997b780bb551baffa14151d7b38e699c9893b952
Mac_Developer <araozangel842@gmail.com>
2023-02-23 10:23:04 +0200

    Add context provider

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ThemeProvider } from './context/context';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
@@ -8,15 +9,19 @@ import Navigation from './navigation';
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  console.log("🚀 ~ file: App.tsx:11 ~ App ~ colorScheme:", colorScheme)
+  
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
+      <ThemeProvider>
+        <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
       </SafeAreaProvider>
+      </ThemeProvider>
     );
   }
 }
