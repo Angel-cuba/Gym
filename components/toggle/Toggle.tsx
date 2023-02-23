1fa665f4554588ad86755ece1a3b2af81ec093be
Mac_Developer <araozangel842@gmail.com>
2023-02-23 17:25:24 +0200

    Refactoring code

--- components/toggle/Toggle.tsx
+++ components/toggle/Toggle.tsx
@@ -1,7 +1,7 @@
-import { View, Text, Pressable, useColorScheme } from 'react-native'
-import React from 'react'
-import { FontAwesome } from '@expo/vector-icons'
-import Colors from '../../constants/Colors'
+import { View, Text, Pressable, useColorScheme } from 'react-native';
+import React from 'react';
+import { FontAwesome } from '@expo/vector-icons';
+import Colors from '../../constants/Colors';
 import { useTheme } from '../../context/context';
 
 export default function Toggle() {
@@ -10,18 +10,27 @@ export default function Toggle() {
     setTheme((theme) => (theme === 'dark' ? 'light' : 'dark'));
   };
   return (
-      <Pressable
-              onPress={toggleTheme}
-              style={({ pressed }) => ({
-                opacity: pressed ? 0.5 : 1,
-              })}
-            >
-              <FontAwesome
-                name={theme === 'dark' ? 'moon-o' : 'sun-o'}
-                size={25}
-                color={theme === 'dark' ? Colors.light.text : Colors.dark.text}
-                style={{ marginLeft: 15 }}
-              />
-            </Pressable>
-  )
+    <Pressable
+      onPress={toggleTheme}
+      style={({ pressed }) => ({
+        opacity: pressed ? 0.5 : 1,
+      })}
+    >
+      <View
+        style={{
+          width: 50,
+          height: 40,
+          marginLeft: 15,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <FontAwesome
+          name={theme === 'dark' ? 'moon-o' : 'sun-o'}
+          size={25}
+          color={theme === 'dark' ? '#a0a0a0' : '#ffe100'}
+        />
+      </View>
+    </Pressable>
+  );
 }
